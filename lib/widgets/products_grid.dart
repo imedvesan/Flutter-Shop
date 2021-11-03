9bc9588d8a327108964569d0e8c41775a7317534
imedvesan <ioanamedvesan@yahoo.com>
2021-11-03 14:58:43 -0500

    adding shopping cart data

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../providers/product.dart';
+// import '../providers/product.dart';
 import 'package:provider/provider.dart';
 import '../providers/products.dart';
 import './product_item.dart';
